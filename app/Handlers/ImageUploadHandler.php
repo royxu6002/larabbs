492ae9d03b101a2d05f485181177e87b14323ed0
vagrant <vagrant@homestead>
2021-03-21 08:27:18 +0000

    avatar limit max

--- app/Handlers/ImageUploadHandler.php
+++ app/Handlers/ImageUploadHandler.php
@@ -2,11 +2,12 @@
 namespace App\Handlers;
 
 use Illuminate\Support\Str;
+use Image;
 
 class ImageUploadHandler {
     protected $img_ext = ['png', 'gif', 'jpg', 'jpeg'];
 
-    public function save($file, $folder, $file_prefix)
+    public function save($file, $folder, $file_prefix, $max_width = false)
     {
         $fold_name =  "uploads/images/".$folder."/".date("ym/d", time());
 
@@ -22,9 +23,25 @@ class ImageUploadHandler {
 
         $file->move($upload_path, $fileName);
 
+        if( $max_width && $extension != 'gif') {
+            $this->reduceSize($upload_path.'/'.$fileName, $max_width);
+        }
+
         return [
             'path' => config('app.url')."/".$fold_name."/".$fileName
         ];
 
+    }
+
+    public function reduceSize($file_path, $max_width)
+    {
+        $image = Image::make($file_path);
+
+        $image->resize($max_width, null, function($constraint) {
+            $constraint->aspectRatio();
+            $constraint->upsize();
+        });
+
+        $image->save();
     }
 }
--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -25,7 +25,7 @@ class UsersController extends Controller
 
         if( $request->avatar) {
             $result = $uploader->save($request->avatar,
-                'avatars', $user->id);
+                'avatars', $user->id, 416);
 
             if($result) {
                 $data['avatar'] = $result['path'];
